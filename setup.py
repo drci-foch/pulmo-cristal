dd90f039177e00c1b82d79f864a770b697ddc1ed
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2025-04-08 13:34:48 +0100

    change camelot setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "PyPDF2>=3.0.0",
-        "camelot-py[cv]>=0.10.1",
+        "camelot-py>=0.10.1",
         "numpy>=1.20.0",
         "pandas>=1.3.0",
     ],
